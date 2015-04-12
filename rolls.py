9665f936539acb817d2562ec1c85f870626b8dd0
Daniel Lamblin <dlamblin+github@gmail.com>
2015-04-12 16:59:43 -0400

    Factor out paired answers and print results

--- rolls.py
+++ rolls.py
@@ -34,6 +34,11 @@ def lens(ss):
     return [len(x[1]) for x in ss]
 
 
+def answers(string, pairs, function, sep=' '):
+    for pair in pairs:
+        print(string.format(pair[0]), function(pair[1]), sep=sep)
+
+
 def main(total_rolls=20000):
     rolls_list = rolls(total_rolls, 1, 6)
     sliced_sum20 = slicedSums(20, rolls_list)
@@ -42,40 +47,25 @@ def main(total_rolls=20000):
     sliced_sum10k = slicedSums(10000, rolls_list)
     sums10k = sums(sliced_sum10k, -10000)
     rollcount10k = lens(sliced_sum10k)
+    pairedsums = [(20, sums20), (10000, sums10k)]
+    pairedrolls = [(20, rollcount20), (10000, rollcount10k)]
 
-    print("Mean of the sum - 20 when M is 20:",
-          statistics.mean(sums20))
-
-    print("Mean of the sum - 10000 when M is 10000:",
-          statistics.mean(sums10k))
-
-    print("Mean of the number of rolls when M is 20:",
-          statistics.mean(rollcount20))
-
-    print("Mean of the number of rolls when M is 10000:",
-          statistics.mean(rollcount10k))
-
-    print("Standard deviation of the sum - 20 when M is 20:",
-          statistics.stdev(sums20))
-
-    print("Standard deviation of the sum - 10000 when M is 10000:",
-          statistics.stdev(sums10k))
-
-    print("Standard deviation of the number of rolls when M is 20:",
-          statistics.stdev(rollcount20))
-
-    print("Standard deviation of the number of rolls when M is 10000:",
-          statistics.stdev(rollcount10k))
-
-    print("\nView of the rolls summing to 20")
-    print(format("Count", ">7"), format("Sum", ">7"), "Rolls", sep='')
-    for s in sliced_sum20:
-        print(format(len(s[1]), ">8"), format(s[0], ">8"), s[1], sep='')
-
-    print("\nView of the rolls summing to 10000")
-    print(format("Count", ">7"), format("Sum", ">7"), "Rolls")
-    for s in sliced_sum10k:
-        print(format(len(s[1]), ">7"), format(s[0], ">7"), s[1])
+    answers("Mean of the sum - {0} when M is {0}:",
+            pairedsums, lambda s: statistics.mean(s))
+    answers("Mean of the number of rolls when M is {0}:",
+            pairedrolls, lambda s: statistics.mean(s))
+    answers("Standard deviation of the sum - {0} when M is {0}:",
+            pairedsums, lambda s: statistics.stdev(s))
+    answers("Standard deviation of the number of rolls when M is {0}:",
+            pairedrolls, lambda s: statistics.stdev(s))
+    answers("\nView of the rolls summing to {0}\n" +
+            format("Count", ">7") + " " + format("Sum", ">7") + " Rolls\n",
+            [(20, sliced_sum20), (10000, sliced_sum10k)],
+            lambda ss: ''.join(
+                format(len(s[1]), ">7") + " " + format(s[0], ">7") + " " +
+                format(s[1]) + "\n" for s in ss)
+            , sep=''
+            )
 
 
 main(100000)
