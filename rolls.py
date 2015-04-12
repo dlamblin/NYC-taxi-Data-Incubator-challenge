700fffd08c8d8ff37a7b20f09786f65db7aa49f5
Daniel Lamblin <dlamblin+github@gmail.com>
2015-04-12 15:54:51 -0400

    Add slicing by sums

--- rolls.py
+++ rolls.py
@@ -1,9 +1,33 @@
 __author__ = 'dlamblin'
+import random
+import sys
+
+if sys.version_info < ( 3, 4):
+    sys.exit("This script requires Python 3.4 or newer!")
 
 
-def rolls(n):
-    r, b = [], 0
-    while b < n:
-        r.append(0)
+def rolls(n, low, high):
+    random.seed(__author__)  # Keeping it reproducible for ease of development
+    r = []
+    for i in range(n):
+        r.append(random.randint(low, high))
     return r
 
+
+def slicedSums(m, r):
+    ss, i, s = [], 0, 0
+    for j in range(len(r)):
+        s += r[j]
+        if s >= m:
+            ss.append([s, r[i:j]])
+            i, s = j + 1, 0
+    return ss
+
+
+def main(total_rolls=20000):
+    print("start up")
+    rolls_list = rolls(total_rolls, 1, 6)
+    sliced_sum20 = slicedSums(20, rolls_list)
+
+
+main(100000)
