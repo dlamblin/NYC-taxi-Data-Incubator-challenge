92b7302f0eedd8a21251f8649caca3519b6783f0
Daniel Lamblin <dlamblin+github@gmail.com>
2015-04-12 21:01:50 -0400

    Setting up CSVs for merged view.

---
+++ taxi.py
@@ -0,0 +1,8 @@
+__author__ = 'dlamblin'
+import pandas
+
+trip_data = pandas.read_csv('trip_data_3.csv', skipinitialspace=True, dtype={'rate_code': str}, parse_dates=[6, 7])
+trip_fare = pandas.read_csv('trip_fare_3.csv', skipinitialspace=True, parse_dates=[4])
+taxis = pandas.merge(trip_data, trip_fare, on=['medallion', 'hack_license', 'pickup_datetime'])
+
+print(taxis[:3])
