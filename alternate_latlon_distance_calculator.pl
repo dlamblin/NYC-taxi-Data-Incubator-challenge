dffbcfaf9c5d52ff66e471823a7e8581b3e1ee80
Daniel Lamblin <dlamblin+github@gmail.com>
2015-04-13 05:31:19 -0400

    Clearer header

--- alternate_latlon_distance_calculator.pl
+++ alternate_latlon_distance_calculator.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 #Simply put, the .apply function in pandas acts pretty slowly to generate one new column based on 4 existing ones.
 #Perl can add this to the file much more simply. Really. And then it's done.
-#perl -MMath::Trig -e '$_=<>;s/dropoff_latitude/dropoff_latitude, trip_latlon_distance/;print;sub hvsn{my($t1,$n1,$t2,$n2)=map(deg2rad($_), @_);return 3960*2*asin(sqrt(sin(($t2-$t1)/2)**2+cos($t1)*cos($t2)*sin(($n2-$n1)/2)**2));}while(<>){my @row=split(/,/);my $miles=hvsn(@row[10..13]);s/.$/,$miles$&/;print}' trip_data_3.csv > trip_data_3_trip_latlon_dist.csv
+#perl -MMath::Trig -e '$_=<>;s/.$/, trip_latlon_distance$&/;print;sub hvsn{my($t1,$n1,$t2,$n2)=map(deg2rad($_), @_);return 3960*2*asin(sqrt(sin(($t2-$t1)/2)**2+cos($t1)*cos($t2)*sin(($n2-$n1)/2)**2));}while(<>){my @row=split(/,/);my $miles=hvsn(@row[10..13]);s/.$/,$miles$&/;print}' trip_data_3.csv > trip_data_3_trip_latlon_dist.csv
 
 use Math::Trig;
 $_=<>; # Modify the header row
-s/dropoff_latitude/dropoff_latitude, trip_latlon_distance/;
+s/.$/, trip_latlon_distance$&/;
 print;
 sub hvsn{ # t:lat n:long
     my ($t1, $n1, $t2, $n2) = map(deg2rad($_), @_);
