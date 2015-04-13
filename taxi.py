cbf83db047516494a92e9951ad3d2cb565d83793
Daniel Lamblin <dlamblin+github@gmail.com>
2015-04-12 21:25:45 -0400

    De-dup vendor id

--- taxi.py
+++ taxi.py
@@ -3,6 +3,6 @@ import pandas
 
 trip_data = pandas.read_csv('trip_data_3.csv', skipinitialspace=True, dtype={'rate_code': str}, parse_dates=[6, 7])
 trip_fare = pandas.read_csv('trip_fare_3.csv', skipinitialspace=True, parse_dates=[4])
-taxis = pandas.merge(trip_data, trip_fare, on=['medallion', 'hack_license', 'pickup_datetime'])
+taxis = pandas.merge(trip_data, trip_fare, on=['medallion', 'hack_license', 'vendor_id', 'pickup_datetime'])
 
 print(taxis[:3])
